2572d776fd1baff177fd6543433b77a4743f9019
rishabh091 <rishabhmalhotra091@gmail.com>
2020-07-22 13:09:49 +0530

    login completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { CompSignupComponent } from './comp-signup/comp-signup.component';
+import { CompLoginComponent } from './comp-login/comp-login.component';
+import { CompHomeComponent } from './comp-home/comp-home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: CompHomeComponent
+  },
+  {
+    path: 'login', component: CompLoginComponent
+  },
+  {
+    path: 'signup', component: CompSignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { CompHomeComponent } from './comp-home/comp-home.component';
+import { CompLoginComponent } from './comp-login/comp-login.component';
+import { CompSignupComponent } from './comp-signup/comp-signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompHomeComponent,
+    CompLoginComponent,
+    CompSignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
