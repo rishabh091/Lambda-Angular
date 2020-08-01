125b5cce15c925332855bbe277d8cdbbbe325fbd
rishabh091 <rishabhmalhotra091@gmail.com>
2020-08-01 14:56:33 +0530

    explore layout completed

--- src/app/comp-explore/comp-explore.component.ts
+++ src/app/comp-explore/comp-explore.component.ts
@@ -9,6 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompExploreComponent implements OnInit {
 
+  previousCat = null;
+  currentCat = 'posts';
+
+  posts = [1,2,3,4,5,6,7,8,9,10,11,12,3,4,5,1,2,3,4,5,6,7];
+  people = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5];
+  questions = [1,2,3,4,5,6,7,8,9,10,11,12,3,4,5,1,2,3,4,5,6,7];
+  projects = [1,2,3,4,5,6,7,8,8,1,1,1,1,1,1,1];
+
   constructor(private serviceAuth: ServiceAuthService, private router: Router) { }
 
   ngOnInit(): void {
@@ -26,6 +34,19 @@ export class CompExploreComponent implements OnInit {
       this.router.navigate(['/firstscreen']);
       return;
     }
+  }
+
+  selectCat(id) {
+    this.previousCat = this.currentCat;
+    this.currentCat = id;
+
+    const previous = document.getElementById(this.previousCat);
+    previous.classList.remove('badge-success');
+    previous.classList.add('badge-secondary');
+
+    const current = document.getElementById(this.currentCat);
+    current.classList.remove('badge-secondary');
+    current.classList.add('badge-success');
   }
 
 }
